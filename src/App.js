e62f2e7341896d7191b4722431bbb122c63b496c
SamanthaJWheeler <115271437+SamanthaJWheeler@users.noreply.github.com>
2023-02-12 14:27:05 -0500

    add validation

--- src/App.js
+++ src/App.js
@@ -7,29 +7,49 @@ import {useFormik} from 'formik'
 function App() {
   const formik = useFormik({
     initialValues: {
-      name:'',
       email:'',
       password:''
     },
     onSubmit: values =>{
-      console.log('form:', values);
-    }
+      alert("Login Sucessful");
+    },
+    validate: (values) => {
+      const errors = {};
+        if (!values.email) errors.email = 'Field Required';
+        if (!values.password) errors.password = 'Field Required';
+        return errors;
+      },
   });
+
   // TODO: add a const called formik assigned to useFormik()
 
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
-        <div>Name</div>
-        <input name="name" type="text" onChange={formik.handleChange} 
-        value={formik.values.name}/>
-        <div>Email</div>
-        <input name="email" type="text" onChange={formik.handleChange} 
-        value={formik.values.email}/>
+        <div>Email:</div>
+        <input 
+          id="emailField"
+          name="email"
+          type="text"
+          onChange={formik.handleChange} 
+          value={formik.values.email}
+        />
         <div>Password</div>
-        <input name="password" type="text" onChange={formik.handleChange} 
-        value={formik.values.password}/>        
-        <button type="submit">submit</button>
+        <input 
+          id="pswField"
+          name="password" 
+          type="text" 
+          onChange={formik.handleChange} 
+          value={formik.values.password}
+        />        
+        <br />
+        {formik.errors.password ? (
+          <div id="pswError" style={{color: "red"}}>
+            {formik.errors.password}
+          </div>
+        ) : null}
+    
+        <button id="submitBtn" type="submit">submit</button>
       </form>
     </div>
   );
