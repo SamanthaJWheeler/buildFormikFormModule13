157bff60dde8b7634f3aecb67e67f50a9a5ee27b
SamanthaJWheeler <115271437+SamanthaJWheeler@users.noreply.github.com>
2023-02-12 14:08:31 -0500

    formik install

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from "react";
+
+import {useFormik} from 'formik'
+
 // TODO: import useFormik from formik library
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name:'',
+      email:'',
+      password:''
+    },
+    onSubmit: values =>{
+      console.log('form:', values);
+    }
+  });
   // TODO: add a const called formik assigned to useFormik()
 
   return (
     <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Name</div>
+        <input name="name" type="text" onChange={formik.handleChange} 
+        value={formik.values.name}/>
+        <div>Email</div>
+        <input name="email" type="text" onChange={formik.handleChange} 
+        value={formik.values.email}/>
+        <div>Password</div>
+        <input name="password" type="text" onChange={formik.handleChange} 
+        value={formik.values.password}/>        
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
